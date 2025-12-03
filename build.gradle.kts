047daea5d473901053584bbca57a1365b55a7c96
Vikindor <vikindor@proton.me>
2025-12-03 16:58:17 +0300

    chore: update dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0-M2"))
+    testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("io.rest-assured:rest-assured:5.5.6")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
